073e45a282701fd49bda9a9bd9ecf4924478a373
Daniel da Silva Filho <danielbrumdasilva@gmail.com>
2022-04-29 13:47:34 -0300

    adicionando template default

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
+import App from './App';
+import './index.css'
+
+import {deepPurple, grey} from '@material-ui/core/colors/'
+
+const theme = createMuiTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: deepPurple['A400'],
+      darker: '#053e85',
+    },
+    secondary: {
+      main: grey[900],
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
